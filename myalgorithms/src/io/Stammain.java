02c56fd64c5020928d5c6d536964901c30311ce7
timi1990 <maximpetrov55@gmail.com>
2015-09-03 16:41:58 +0300

    stam

--- myalgorithms/src/io/Stammain.java
+++ myalgorithms/src/io/Stammain.java
@@ -1,5 +1,10 @@
 package io;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
 import algorithms.mazeGenerators.InitMazeArguments;
 import algorithms.mazeGenerators.Maze3d;
 import algorithms.mazeGenerators.MyMaze3dGenerator;
@@ -8,14 +13,22 @@ public class Stammain {
 
 	public static void main(String[] args) {
 		InitMazeArguments initMazeArguments= new InitMazeArguments();
-		initMazeArguments.set_rows(10);         
-		initMazeArguments.set_columns(10);
-		initMazeArguments.set_dimension(4);
+		initMazeArguments.set_rows(5);         
+		initMazeArguments.set_columns(5);
+		initMazeArguments.set_dimension(3);
 		Maze3d maze=new MyMaze3dGenerator().generate(initMazeArguments);
 		maze.PrintMaze();
-		byte[] stam=maze.toByteArray();
-		Maze3d other=new Maze3d(stam);
-		other.PrintMaze();
+		try {
+			OutputStream out=new MyCompressorOutputStream(new FileOutputStream("maze.txt"));
+			out.write(maze.toByteArray());
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+				
+
 
 	}
 
